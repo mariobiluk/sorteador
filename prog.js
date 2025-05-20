d70910405c86a6607d96f09b7f0643b6678f72c9
MARIO TEODORO DE MELLO BILUK <mario_biluk@estudante.sesisenai.org.br>
2025-05-20 10:04:05 -0300

    versao1

--- prog.js
+++ prog.js
@@ -6,18 +6,32 @@ function fazerSorteio(min, max) {
 }
 
 function gerarNum() {
-    let num = Number(document.getElementById('num').value)
-    console.log(num)
+    let min = Number(document.getElementById('min').value)
+    let max = Number(document.getElementById('max').value)
+    let qtd = Number(document.getElementById('qtd').value)
+    console.log(qtd)
+    console.log(min)
+    console.log(max)
 
-    let valor = 0
+    let valor = 0 //recebe valor do sorteio
     res.innerHTML = ``
     let soma = 0
+    let media = 0.0
 
-    for (let i = 0; i < num; i++) {
-        valor = fazerSorteio(1, 10)
-        soma+= valor
-        res.innerHTML += `O número gerado foi: ${valor}<br>`
+    if ((min < 0 || max > 100) && (qtd < 0 || qtd > 10)) {
+        res.innerHTML = `Valores inválidos. Digite novamente!`
+        res.innerHTML += `O menor valor permitido é 1 e o maior é 100!`
+    } else {
+        for (let i = 0; i < qtd; i++) {
+            valor = fazerSorteio(min, max)
+            soma += valor
+            res.innerHTML += `O número gerado foi: ${valor}<br>`
+        }
+        media = soma / qtd
+        res.innerHTML += `A soma dos números aleatórios foi: ${soma}<br>`
+        res.innerHTML += `A media dos números aleatórios foi: ${media}<br>`
+
     }
 
-    res.innerHTML+=`A soma dos números aleatórios foi: ${soma}<br>`
+
 }
